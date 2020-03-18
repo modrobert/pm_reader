a023ab4dcd771719072b96a38d1af28c32cea81a
modrobert <modrobert@gmail.com>
2020-03-18 10:26:45 +0700

    removed whitespace

--- pm_reader.py
+++ pm_reader.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2020  Robert V. <modrobert@gmail.com> 
+# Copyright (C) 2020  Robert V. <modrobert@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
